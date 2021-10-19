90ee480133d4f18c757ff79a2ea007b0259ac5e8
balakelly459 <21192911@CFP127ALUNO.LOCAL>
2021-10-19 08:44:35 -0300

    first commit

---
+++ C2/index.js
@@ -0,0 +1,13 @@
+const nome = 'Fernando Leonid'
+
+const aluno = {
+    nome: 'Hugo',
+    idade: 16,
+    nota: 8,
+}
+
+function soma(a, b) {
+    return a + b
+}
+
+const cadastroFornecedoresInformatica = (fornecedor) => insert(fornecedor.nome, fornecedor.produtos, fornecedor.contato)
